b270f8f74c1c77a30a3989b935796c8174b4e973
Kamlesh Malvi <kmalvi2012@gmail.com>
2026-06-12 09:19:22 +0530

    practice code

---
+++ app.js
@@ -0,0 +1,7 @@
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+// examples
+console.log(reverseString("hello")); 
+console.log(reverseString("A man a plan a canal")); 
